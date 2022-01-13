c65a78d661f505de38504209c3c0c7f9dec31f7b
DawonJeon <dawon3024@naver.com>
2022-01-13 20:01:39 +0900

    everyday git commit

---
+++ 220113/script.js
@@ -0,0 +1,11 @@
+
+
+// const body = document.body
+// const div = document.createElement("div")
+// body.append(div)
+
+// // div.innerText = "Hello Worlds"
+// div.textContent = "Hello World3"
+
+
+document.querySelector
